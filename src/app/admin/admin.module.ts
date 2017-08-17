652708983f8d71c2549be6091c41b99af3347bdc
Victor_Lo <694978795@qq.com>
2017-08-17 09:47:26 +0800

    整理包

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,7 +4,7 @@ import { ModalModule, PopoverModule, TabsModule, TooltipModule } from 'ngx-boots
 
 import { AdminRoutingModule } from './admin-routing.module'
 import { AppCommonModule } from '@app/shared/common/app-common.module'
-import { AppStorageService } from 'shared/services/storage.service';
+import { AppStorageService } from "shared/services/storage.service";
 import { BaseInfoComponent } from './manage-booking/create-or-edit-booking/base-info/base-info.component';
 import { BookingCustomModelComponent } from './manage-booking/booking-list/booking-custom-model/booking-custom-model.component';
 import { BookingListComponent } from './manage-booking/booking-list/booking-list.component';
@@ -14,13 +14,14 @@ import { ConfirmOrderModelComponent } from './manage-booking/booking-list/confir
 import { ContactInfoComponent } from './manage-org/create-or-edit-outlet/contact-info/contact-info.component';
 import { CreateOrEditBookingComponent } from './manage-booking/create-or-edit-booking/create-or-edit-booking.component';
 import { CreateOrEditOutletComponent } from './manage-org/create-or-edit-outlet/create-or-edit-outlet.component';
-import { CustomerForEditModelComponent } from 'app/admin/manage-customer/customer-list/customer-for-edit-model/customer-for-edit-model.component';
+import { CustomerForEditModelComponent } from "app/admin/manage-customer/customer-list/customer-for-edit-model/customer-for-edit-model.component";
 import { CustomerListComponent } from './manage-customer/customer-list/customer-list.component';
+import { DateInputModule } from '@progress/kendo-angular-dateinputs';
 import { DialogModule } from '@progress/kendo-angular-dialog';
 import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
 import { FileUploadModule } from '@node_modules/ng2-file-upload';
-import { GetUserForEdit } from 'shared/services/get-user-info.service';
-import { HostSettingService } from 'shared/services/get-host-settings.service';
+import { GetUserForEdit } from "shared/services/get-user-info.service";
+import { HostSettingService } from "shared/services/get-host-settings.service";
 import { NgModule } from '@angular/core';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { OrgInfoComponent } from './manage-org/org-info/org-info.component';
@@ -30,12 +31,12 @@ import { PaginationComponent } from './shared/pagination/pagination.component';
 import { PictureManageComponent } from './manage-booking/create-or-edit-booking/picture-manage/picture-manage.component';
 import { QRCodeModule } from 'angular2-qrcode';
 import { ShareBookingModelComponent } from './manage-booking/create-or-edit-booking/share-booking-model/share-booking-model.component';
-import { TenantService } from 'shared/services/tenant.service';
+import { TenantService } from "shared/services/tenant.service";
 import { TimeInfoComponent } from './manage-booking/create-or-edit-booking/time-info/time-info.component';
 import { TipsComponent } from './shared/tips/tips.component';
 import { UploadOrgBgComponent } from './manage-org/org-info/upload-org-bg/upload-org-bg.component';
 import { UploadOrgLogoComponent } from './manage-org/org-info/upload-org-logo/upload-org-logo.component';
-import { UploadPictureGalleryComponent } from 'app/admin/shared/upload-picture-gallery/upload-picture-gallery.component';
+import { UploadPictureGalleryComponent } from "app/admin/shared/upload-picture-gallery/upload-picture-gallery.component";
 import { UploadPictureNoneGalleryComponent } from './shared/upload-picture-none-gallery/upload-picture-none-gallery.component';
 import { UtilsModule } from '@shared/utils/utils.module'
 
@@ -59,6 +60,7 @@ import { UtilsModule } from '@shared/utils/utils.module'
         ButtonsModule,
         SharedModule,
         DialogModule,
+        DateInputModule,
         QRCodeModule,
         NgxPaginationModule
     ],
