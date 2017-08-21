7408ed3aa31aa6a2f633a21a00d22f49f5172560
Zhangxu <2590000714@qq.com>
2017-08-21 16:22:36 +0800

    补交文件

--- src/app/admin/manage-booking/create-or-edit-booking/picture-manage/picture-manage.component.ts
+++ src/app/admin/manage-booking/create-or-edit-booking/picture-manage/picture-manage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Injector, ViewChild, Output, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, Injector, Input, OnInit, Output, ViewChild } from '@angular/core';
+
 import { AppComponentBase } from "shared/common/app-component-base";
 import { BookingPictureEditDto } from "shared/service-proxies/service-proxies";
 import { UploadPictureGalleryComponent } from 'app/admin/shared/upload-picture-gallery/upload-picture-gallery.component';
@@ -43,6 +44,22 @@ export class PictureManageComponent extends AppComponentBase implements OnInit {
     pictureForEdit.displayOrder = this.displayOrder++; //暂时测试
     this.allPictureEdit.push(pictureForEdit);
     this.sendAllPictureForEdit.emit(this.allPictureEdit);
+  }
+
+  deletePic(pictureIndex: number): void {
+        this.removeArrayValue(this.allPictureEdit, pictureIndex);
+  }
+
+  changePic(pictureIndex: number): void {
+      this.show();
+  }
+
+  // 移除数组某个索引的值
+  private removeArrayValue(arr: any, index: number): void {
+        if (arr.length <= 0) {
+            return;
+        }
+        arr.splice(index, 1);
   }
 
 }
