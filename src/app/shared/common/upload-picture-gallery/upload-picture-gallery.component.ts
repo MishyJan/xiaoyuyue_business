414e41fb79acc4c113a8a5de4fa1ca246d62608c
Zhangxu <2590000714@qq.com>
2017-08-26 11:49:46 +0800

    图片库功能与UI实现

--- src/app/shared/common/upload-picture-gallery/upload-picture-gallery.component.ts
+++ src/app/shared/common/upload-picture-gallery/upload-picture-gallery.component.ts
@@ -1,32 +1,48 @@
 import '@node_modules/qiniu-js/dist/qiniu.min';
 
-import { BookingPictureEditDto, PictureServiceProxy, UpdateProfilePictureInput, UploadTokenOutput } from 'shared/service-proxies/service-proxies';
-import { Component, EventEmitter, Injector, OnInit, Output, ViewChild } from '@angular/core';
+import { BookingPictureEditDto, IPictureGroupListDto, PagedResultDtoOfPictureListDto, PictureGroupListDto, PictureListDto, PictureServiceProxy, UpdateProfilePictureInput, UploadTokenOutput } from 'shared/service-proxies/service-proxies';
+import { Component, EventEmitter, Injector, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { FileUploader, FileUploaderOptions } from 'ng2-file-upload';
 
 import { AppComponentBase } from 'shared/common/app-component-base';
 import { AppConsts } from 'shared/AppConsts';
+import { BaseGridDataInputDto } from 'shared/grid-data-results/base-grid-data-Input.dto';
 import { IAjaxResponse } from 'abp-ng2-module/src/abpHttp';
 import { ModalDirective } from 'ngx-bootstrap';
 import { TokenService } from 'abp-ng2-module/src/auth/token.service';
+import { accountModuleAnimation } from '@shared/animations/routerTransition';
+import { element } from 'protractor';
 
 @Component({
     selector: 'xiaoyuyue-upload-picture-gallery',
     templateUrl: './upload-picture-gallery.component.html',
-    styleUrls: ['./upload-picture-gallery.component.scss']
+    styleUrls: ['./upload-picture-gallery.component.scss'],
+    animations: [accountModuleAnimation()]
 })
+
 export class UploadPictureGalleryComponent extends AppComponentBase implements OnInit {
-    @Output() getAllPictureUrl: EventEmitter<SafeUrl[]> = new EventEmitter();
-    @Output() sendPictureForEdit: EventEmitter<BookingPictureEditDto> = new EventEmitter();
-    @ViewChild('uploadPictureModel') modal: ModalDirective;
+    selectedPicIndex: number;
+    selectedPicIndexArr: boolean[] = [];
+
+    groupActiveIndex: number = 0;
+    picGroupItemData: PictureListDto[];
+    groupId: number;
+    defaultPicGalleryGroupId: number;
+    picGalleryGroupData: PictureGroupListDto[];
+    gridParam: BaseGridDataInputDto = new BaseGridDataInputDto();
 
     loading = false;
     tabToggle = true;
 
+    // 记录已选中图片的数据
+    public selectedPicData: PictureListDto[] = [];
+    // 最大上传图片数量
+    public maxPicNum = 4;
     public allPictureUrl: SafeUrl[] = [];
     public allPictureId: number[] = [];
     public pictureForEdit: BookingPictureEditDto = new BookingPictureEditDto();
+    public picGalleryForEdit: BookingPictureEditDto[] = [];
     public uploader: FileUploader;
     public temporaryPictureUrl: string;
     public safeTemporaryPictureUrl: SafeUrl;
@@ -34,6 +50,12 @@ export class UploadPictureGalleryComponent extends AppComponentBase implements O
 
     private temporaryPictureFileName: string;
     private _$profilePicture: JQuery;
+
+    @Input() existingPicNum: number;
+    @Output() getAllPictureUrl: EventEmitter<SafeUrl[]> = new EventEmitter();
+    @Output() sendPictureForEdit: EventEmitter<BookingPictureEditDto> = new EventEmitter();
+    @Output() sendPicGalleryForEdit: EventEmitter<BookingPictureEditDto[]> = new EventEmitter();
+    @ViewChild('uploadPictureModel') modal: ModalDirective;
 
     constructor(
         injector: Injector,
@@ -46,9 +68,105 @@ export class UploadPictureGalleryComponent extends AppComponentBase implements O
 
     ngOnInit() {
     }
+
     /* 上传图片时的索引值，用于更换某个索引值的图片数据 */
     show(): void {
         this.modal.show();
+        this.loadPicGalleryData();
+        this.loadAllPicAsync();
+    }
+
+    // 获取图片库所有分组数据
+    loadPicGalleryData(): void {
+        this._pictureServiceProxy
+            .getPictureGroupAsync()
+            .subscribe(result => {
+                this.picGalleryGroupData = result;
+                this.groupId = this.defaultPicGalleryGroupId = result[0].id;
+            })
+    }
+
+    // 根据分组ID获取某分组下所有图片数据
+    loadAllPicAsync(): void {
+        this._pictureServiceProxy
+            .getPictureAsync(
+            this.groupId,
+            this.gridParam.GetSortingString(),
+            this.gridParam.MaxResultCount,
+            this.gridParam.SkipCount
+            )
+            .subscribe(result => {
+                this.picGroupItemData = result.items;
+            })
+    }
+
+    public confirmGallerySelect(): void {
+        // 把数据传给父组件
+        this.sendPicGalleryForEdit.emit(this.picGalleryForEdit);
+        this.close();
+    }
+
+    // 选择图片库图片，保存选中数据
+    public selectPicHandler(index: number): void {
+        // 可选择的数量
+        let electiveNum = this.maxPicNum - this.existingPicNum;
+        let selectedNum = 0;
+
+        this.selectedPicData = [];
+        this.picGalleryForEdit = [];
+
+        this.selectedPicIndex = index;
+        this.selectedPicIndexArr[index] = !this.selectedPicIndexArr[index];
+
+        // 获取图片分组数据
+        this.selectedPicIndexArr.forEach((element, inx) => {
+            if (element === true) {
+                if (this.selectedPicData.length >= 4) {
+                    return;
+                }
+                this.selectedPicData.push(this.picGroupItemData[inx]);
+            } else {
+                this.selectedPicData.splice(inx, 1);
+            }
+        });
+
+        // 将图片分组数据转DTO
+        this.selectedPicData.forEach((element, inx) => {
+            let temp = new BookingPictureEditDto();
+            // temp.displayOrder = inx;
+            temp.pictureId = element.id;
+            temp.pictureUrl = element.originalUrl;
+            this.picGalleryForEdit.push(temp);
+        });
+
+        // 图片数量超过限制警告
+        if (this.setPicElectiveNum() < 0) {
+            this.selectedPicIndexArr[index] = !this.selectedPicIndexArr[index];
+            this.message.warn('图片超过上限');
+        }
+
+    }
+
+    // 获取可选择图片的数量
+    public setPicElectiveNum(): number {
+        return this.maxPicNum - this.existingPicNum - this.setPicSelectNum();;
+    }
+
+    // 获取选中图片数组中，返回已选择的数据长度
+    public setPicSelectNum(): number {
+        let length = 0;
+        this.selectedPicIndexArr.forEach(element => {
+            if (element === true) {
+                length++;
+            }
+        });
+        return length;
+    }
+
+    public groupItemActive(groupItem: IPictureGroupListDto, groupActiveIndex: number): void {
+        this.groupActiveIndex = groupActiveIndex;
+        this.groupId = groupItem.id;
+        this.loadAllPicAsync();
     }
 
     initFileUploader(): void {
@@ -183,6 +301,9 @@ export class UploadPictureGalleryComponent extends AppComponentBase implements O
 
     close(): void {
         this.modal.hide();
+        if (this.tabToggle) {
+            return;
+        }
         this.picturyDestroy();
     }
 
@@ -194,7 +315,7 @@ export class UploadPictureGalleryComponent extends AppComponentBase implements O
         this.tabToggle = false;
         this.initFileUploader();
     }
-
+    // 上传图销毁
     picturyDestroy(): void {
         this.pictureForEdit = new BookingPictureEditDto();
         this._$profilePicture.removeAttr('src');
