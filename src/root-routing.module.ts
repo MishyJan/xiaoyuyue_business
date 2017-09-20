dede73352bf291eaf907584464b5775e9ff07dd8
Victor_Lo <694978795@qq.com>
2017-09-20 11:32:04 +0800

    解决面包屑功能在页面刷新时无法剩下的问题

--- src/root-routing.module.ts
+++ src/root-routing.module.ts
@@ -1,5 +1,6 @@
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 
+import { BreadcrumbService } from 'shared/services/bread-crumb.service';
 import { ExternalLoginGuard } from 'app/shared/common/auth/external-login-guard';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './app/shared/layout/page-not-found/page-not-found.component';
@@ -18,4 +19,9 @@ const routes: Routes = [
     exports: [RouterModule],
     providers: []
 })
-export class RootRoutingModule { }
+export class RootRoutingModule {
+    // 提前 注册 BreadcrumbService
+    constructor(private breadcrumbService: BreadcrumbService) {
+
+    }
+}
