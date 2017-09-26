6a461ce63d0f9fec0829446999e3053027a9953d
Zhangxu <2590000714@qq.com>
2017-09-26 17:21:44 +0800

    圆形进度条功能实现

---
+++ src/app/shared/common/indicator/indicator.directive.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { IndicatorDirective } from './indicator.directive';
+
+describe('Directive: Indicator', () => {
+  it('should create an instance', () => {
+    const directive = new IndicatorDirective();
+    expect(directive).toBeTruthy();
+  });
+});
