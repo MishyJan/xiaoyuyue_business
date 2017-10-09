bb36a3b7d2fb0100179eb06f04734f05d7332642
Victor_Lo <694978795@qq.com>
2017-10-09 18:28:41 +0800

    优化移动端请求数量

--- src/app/account-settings/security/phone/phone.component.ts
+++ src/app/account-settings/security/phone/phone.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, Injector, ElementRef, ViewChild } from '@angular/core';
-import { AppComponentBase } from '@shared/common/app-component-base';
-import { VerificationCodeType } from 'shared/AppEnums';
-import { Router } from '@angular/router';
-import { Location } from '@angular/common';
 import { BindingPhoneNumInput, ChangeBindingPhoneNumInput, CheckUserCodeInput, CodeSendInput, ProfileServiceProxy, SMSServiceProxy } from '@shared/service-proxies/service-proxies';
+import { Component, ElementRef, Injector, OnInit, ViewChild } from '@angular/core';
+
+import { AppComponentBase } from '@shared/common/app-component-base';
+import { AppSessionService } from 'shared/common/session/app-session.service';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { VerificationCodeType } from 'shared/AppEnums';
 import { accountModuleAnimation } from '@shared/animations/routerTransition';
 
 @Component({
@@ -14,22 +16,12 @@ import { accountModuleAnimation } from '@shared/animations/routerTransition';
 })
 export class PhoneComponent extends AppComponentBase implements OnInit {
 
-    constructor(
-        injector: Injector,
-        private _router: Router,
-        private _location: Location,
-        private _SMSServiceProxy: SMSServiceProxy,
-        private _profileServiceProxy: ProfileServiceProxy
-    ) {
-        super(injector);
-    }
-
     bindingPhoneNumInput: BindingPhoneNumInput = new BindingPhoneNumInput();
-    time: number = 60;
+    time = 60;
     sendSMSTimer: NodeJS.Timer;
     existentPhoneNum: string;
-    isSendNewPhone: boolean = false;
-    isVerified: boolean = false;
+    isSendNewPhone = false;
+    isVerified = false;
     currentPhoneNum: string;
     encryptPhoneNum: string;
     isSendSMS: boolean;
@@ -37,13 +29,22 @@ export class PhoneComponent extends AppComponentBase implements OnInit {
     codeSendInput: CodeSendInput = new CodeSendInput();
     changeBindingPhoneNumInput: ChangeBindingPhoneNumInput = new ChangeBindingPhoneNumInput();
     checkUserCodeInput: CheckUserCodeInput = new CheckUserCodeInput();
-    @ViewChild("smsBtn") smsBtn: ElementRef;
+    @ViewChild('smsBtn') smsBtn: ElementRef;
 
+    constructor(
+        injector: Injector,
+        private _router: Router,
+        private _location: Location,
+        private _SMSServiceProxy: SMSServiceProxy,
+        private _profileServiceProxy: ProfileServiceProxy,
+        private _sessionService: AppSessionService,
+    ) {
+        super(injector);
+    }
 
     ngOnInit() {
         this.getUserPhoneNum();
     }
-
 
     verificationPhoneNum(): void {
         this.checkUserCodeInput.code = this.code;
@@ -63,7 +64,7 @@ export class PhoneComponent extends AppComponentBase implements OnInit {
             .subscribe(() => {
                 this._location.back();
                 setTimeout(() => {
-                    this.notify.success("更绑成功");
+                    this.notify.success('更绑成功');
                 }, 1000);
             });
     }
@@ -75,7 +76,7 @@ export class PhoneComponent extends AppComponentBase implements OnInit {
             .subscribe(() => {
                 this._location.back();
                 setTimeout(() => {
-                    this.notify.success("绑定成功");
+                    this.notify.success('绑定成功');
                 }, 1000);
             });
     }
@@ -87,7 +88,7 @@ export class PhoneComponent extends AppComponentBase implements OnInit {
         }
 
         if (realTimePhoneNum !== this.existentPhoneNum) {
-            this.smsBtn.nativeElement.innerHTML = "发送验证码";
+            this.smsBtn.nativeElement.innerHTML = '发送验证码';
             this.isSendNewPhone = false;
             clearInterval(this.sendSMSTimer);
         } else {
@@ -154,24 +155,19 @@ export class PhoneComponent extends AppComponentBase implements OnInit {
 
             this.isSendSMS = false;
             this.isSendNewPhone = false;
-            event.target.innerHTML = this.l("AgainSendValidateCode");
+            event.target.innerHTML = this.l('AgainSendValidateCode');
         }, 60000);
     }
 
     getUserPhoneNum(): void {
-        this._profileServiceProxy
-            .getCurrentUserProfileForEdit()
-            .subscribe(result => {
-                this.currentPhoneNum = result.phoneNumber;
-                this.encrypt();
-            })
+        this.currentPhoneNum = this._sessionService.user.phoneNumber;
+        this.encrypt();
     }
-
 
     private encrypt(): void {
         if (!this.currentPhoneNum) {
             return;
         }
-        this.encryptPhoneNum = "•••••••" + this.currentPhoneNum.substr(this.currentPhoneNum.length - 4);
+        this.encryptPhoneNum = '•••••••' + this.currentPhoneNum.substr(this.currentPhoneNum.length - 4);
     }
 }
