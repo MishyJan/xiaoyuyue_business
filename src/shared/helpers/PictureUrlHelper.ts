8fe39203bd4164589d6efe79eab89782fa5cdb3c
Zhangxu <2590000714@qq.com>
2017-10-24 15:23:52 +0800

    处理编辑本店picURL为空字符串时的bug

--- src/shared/helpers/PictureUrlHelper.ts
+++ src/shared/helpers/PictureUrlHelper.ts
@@ -2,30 +2,30 @@ import { MediaCompressFormat } from 'shared/AppConsts';
 
 export class PictureUrlHelper {
     static getCompressUrl(url: string, compressProt: string): string {
-        return url + '?' + compressProt;
+        return url === '' ? url : url + '?' + compressProt;
     }
 
     static getBookingListPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.bookingListFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.bookingListFormat;
     }
 
     static getBookingInfoPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.bookingInfoFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.bookingInfoFormat;
     }
 
     static getOutletListPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.outletListFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.outletListFormat;
     }
 
     static getOutletInfoPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.outletInfoFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.outletInfoFormat;
     }
 
     static getContactorPicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.contactorFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.contactorFormat;
     }
 
     static getMinProfilePicCompressUrl(url: string): string {
-        return url + '?' + MediaCompressFormat.minProfilePictureFormat;
+        return url === '' ? url : url + '?' + MediaCompressFormat.minProfilePictureFormat;
     }
 }
