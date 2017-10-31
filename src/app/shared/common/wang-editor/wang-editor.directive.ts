0c9c1e452fbd58840601839df5cb1223f4d22c02
Victor_Lo <694978795@qq.com>
2017-10-31 12:19:57 +0800

    wangedit 添加粘贴面板上传图片

--- src/app/shared/common/wang-editor/wang-editor.directive.ts
+++ src/app/shared/common/wang-editor/wang-editor.directive.ts
@@ -1,6 +1,6 @@
 import * as wangEditor from 'wangeditor/release/wangEditor.js'
 
-import { AfterViewInit, Directive, ElementRef } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 import { AbpSessionService } from '@abp/session/abp-session.service';
 import { PictureServiceProxy } from 'shared/service-proxies/service-proxies';
@@ -12,6 +12,7 @@ import { UploadPictureService } from 'shared/services/upload-picture.service';
 export class WangEditorDirective implements AfterViewInit {
 
   private editor: any;
+  private editorHtml: string;
 
   constructor(private _element: ElementRef,
     private _sessionService: AbpSessionService,
@@ -22,12 +23,16 @@ export class WangEditorDirective implements AfterViewInit {
   ngAfterViewInit(): void {
     this.initEditor();
   }
+
   initEditor() {
     const editordom = this._element.nativeElement;
     this.editor = new wangEditor(editordom)
 
     this.editor.customConfig.uploadImgShowBase64 = true;
     this.editor.customConfig.zIndex = 100;
+    this.editor.customConfig.onchange = this.editorOnChange;
+    // 允许上传到七牛云存储
+    this.editor.customConfig.qiniu = true
     this.editor.customConfig.menus = [
       'head',  // 标题
       'bold',  // 粗体
@@ -150,5 +155,91 @@ export class WangEditorDirective implements AfterViewInit {
   // 封装 console.log 函数
   printLog(title) {
     window.console && console.log(title);
+  }
+
+  /*picBase是base64图片带头部的完整编码*/
+  putb642Qiniu(picBase, editorHtml) {
+
+    /*picUrl用来存储返回来的url*/
+    let picUrl;
+
+    /*把头部的data:image/png;base64,去掉。（注意：base64后面的逗号也去掉）*/
+    picBase = picBase.substring(22);
+
+    /*通过base64编码字符流计算文件流大小函数*/
+    function fileSize(str) {
+      let fileSize;
+      if (str.indexOf('=') > 0) {
+        const indexOf = str.indexOf('=');
+        str = str.substring(0, indexOf);// 把末尾的’=‘号去掉
+      }
+
+      fileSize = str.length - (str.length / 8) * 2;
+      return fileSize;
+    }
+
+    /*把字符串转换成json*/
+    function strToJson(str) {
+      const json = eval('(' + str + ')');
+      return json;
+    }
+
+    const url = 'http://up.qiniu.com/putb64/' + fileSize(picBase);
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () { replaceBase(picBase, editorHtml); }
+
+    function replaceBase(picBase, editorHtml) {
+      if (xhr.readyState === 4) {
+        const keyText = xhr.responseText;
+
+        /*返回的key是字符串，需要装换成json*/
+        const retsult = strToJson(keyText);
+
+        /* http://ojvh6i96g.bkt.clouddn.com/是我的七牛云空间网址，keyText.key 是返回的图片文件名*/
+        picUrl = 'https://image.xiaoyuyue.com/' + retsult.key;
+        editorHtml.replace(picBase, picUrl)
+      }
+    }
+
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('Content-Type', 'application/octet-stream');
+    xhr.setRequestHeader('Authorization', 'UpToken 这里填写你的token（注意前面的UpToken后面有空格）');
+    xhr.send(picBase);
+  }
+
+  editorOnChange(html) {
+    this.editorHtml = html;
+    // html 即变化之后的内容
+    // 思路分两步：作者（yanue）.
+    // 1，匹配出图片img标签（即匹配出所有图片），过滤其他不需要的字符
+    // 2.从匹配出来的结果（img标签中）循环匹配出图片地址（即src属性）
+    // 匹配图片（g表示匹配所有结果i表示区分大小写）
+    const imgReg = /<img.*?(?:>|\/>)/gi;
+    // 匹配src属性
+    const srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
+    const reg = /^\s*data:([a-z]+\/[a-z0-9-+.]+(;[a-z-]+=[a-z0-9-]+)?)?(;base64)?,([a-z0-9!$&',()*+;=\-._~:@\/?%\s]*?)\s*$/i;
+    const arr = html.match(imgReg);
+    alert('所有已成功匹配图片的数组：' + arr);
+    for (let i = 0; i < arr.length; i++) {
+      const src = arr[i].match(srcReg);
+      // 检查图片路径是否是base64
+      const result = reg.test(src[1])
+      if (reg.test(src[1])) {
+        this.putb642Qiniu(src[1], this.editorHtml);
+      }
+
+      // // 获取图片地址
+      // if (src[1]) {
+      //   alert('已匹配的图片地址' + (i + 1) + '：' + src[1]);
+      // }
+      // // 当然你也可以替换src属性
+      // if (src[0]) {
+      //   let t = src[0].replace(/src/i, 'href');
+      //   src.input = src.input.replace(src[1], '');
+      //   alert(src.input);
+      // }
+    }
+
+    this.editor.txt.html(this.editorHtml)
   }
 }
