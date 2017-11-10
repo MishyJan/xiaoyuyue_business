819230e405042cdccd097d3c58826eecb9631f63
Victor_Lo <694978795@qq.com>
2017-11-10 19:48:46 +0800

    解决安卓下相册图片无法上传bug

--- src/app/shared/common/mobile-upload-picture-list/mobile-upload-picture-list.component.ts
+++ src/app/shared/common/mobile-upload-picture-list/mobile-upload-picture-list.component.ts
@@ -77,7 +77,7 @@ export class MobileUploadPictureListComponent extends AppComponentBase implement
                         max_file_size: '5mb',
                         prevent_duplicates: true,
                         mime_types: [
-                            { title: 'Image files', extensions: 'jpg,jpeg,gif,png' },  // 限定jpg,gif,png后缀上传
+                            // { title: 'Image files', extensions: 'jpg,jpeg,gif,png' },  // 限定jpg,gif,png后缀上传
                         ]
                     },
                     x_vars: {
